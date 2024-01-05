fb4cc79559794ee65f3362a90d432dc9d88bc3c2
Giveawaysonline <155429474+Giveawaysonline@users.noreply.github.com>
2024-01-05 01:49:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
 function showPage(pageNumber) {
-  // Hide all pages
   var pages = document.getElementsByClassName('page');
   for (var i = 0; i < pages.length; i++) {
     pages[i].style.display = 'none';
   }
-  // Show the current page
-  document.getElementById('page' + pageNumber).style.display = 'block';
+  var currentPage = document.getElementById('page' + pageNumber);
+  currentPage.style.display = 'block';
   
-  // If we're showing the page that should have the content locker, load it
   if (pageNumber === 3) {
-    loadContentLocker();
+    loadCPAScript();
   }
 }
 
-// This function dynamically injects the CPA content locker script when called
-function loadContentLocker() {
-  var script = document.createElement('script');
-  script.src = "https://singingfiles.com/script_include.php?id=1616243";
-  script.type = "text/javascript";
-  document.body.appendChild(script);
+function loadCPAScript() {
+  if (!document.getElementById('cpaScript')) {
+    var script = document.createElement('script');
+    script.id = 'cpaScript';
+    script.type = 'text/javascript';
+    script.src = 'https://singingfiles.com/script_include.php?id=1617589';
+    document.body.appendChild(script);
+  }
 }
 
-// This function is attached to your 'Join' button's onclick event
 function initJoinProcess() {
-  showPage(2); // Assuming page 2 is where they enter their details
+  showPage(2);
 }
